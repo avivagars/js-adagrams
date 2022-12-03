44dbc435be77a2b886aba699e6604ed7c442fb9a
Andrea-Galindo <andreagalindo.guerra@gmail.com>
2022-12-03 10:40:22 -0500

    added function drawLetters and passes tests wave 01

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,134 +1,86 @@
+const letterPool = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
 
-const letterPool =  {
-  'A': 9, 
-  'B': 2, 
-  'C': 2, 
-  'D': 4, 
-  'E': 12, 
-  'F': 2, 
-  'G': 3, 
-  'H': 2, 
-  'I': 9, 
-  'J': 1, 
-  'K': 1, 
-  'L': 4, 
-  'M': 2, 
-  'N': 6, 
-  'O': 8, 
-  'P': 2, 
-  'Q': 1, 
-  'R': 6, 
-  'S': 4, 
-  'T': 6, 
-  'U': 4, 
-  'V': 2, 
-  'W': 2, 
-  'X': 1, 
-  'Y': 2, 
-  'Z': 1
-}
+const letterValues = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 2,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
 
-const adagramsPoints = {
-  'A': 1,
-  'E': 1, 
-  'I': 1, 
-  'O': 1, 
-  'U': 1, 
-  'L': 1,
-  'N': 1,
-  'R': 1, 
-  'S': 1, 
-  'T': 1,
-  'D': 2,
-  'G': 2,
-  'B': 3, 
-  'C': 3, 
-  'M': 3, 
-  'P': 3,
-  'F': 4,
-  'H': 4, 
-  'V': 4, 
-  'W': 4,
-  'Y': 4,
-  'K': 5,
-  'J': 8,
-  'X': 8,
-  'Q': 10,
-  'Z': 10
+export const drawLetters = () => {
+  let hand = [];
+  let letterPoolList = [];
+
+  // letterPoolList contains all available letters that player can select from
+  for (const [key, value] of Object.entries(letterPool)) {
+    for (let i = 0; i < value; i++) {
+      letterPoolList.push(key);
+    }
   }
 
-
-  export const drawLetters = () => {
-    let hand = [];
-    let letterPoolList = [];
-  
-    // letterPoolList contains all available letters that player can select from
-    for (const [key, value] of Object.entries(letterPool)) {
-      for (let i = 0; i < value; i++) {
-        letterPoolList.push(key);
-      }
-    }
-  
-    // helper function to select a random letter from letterPoolList
-    const selectRandomLetter = (letterPoolList) => {
-      let letter =
-        letterPoolList[Math.floor(Math.random() * letterPoolList.length)];
-      return letter;
-    };
-  
-    // builds a hand of 10 random letters
-    for (let i = 0; i < 10; i++) {
-      const letter = selectRandomLetter(letterPoolList);
-      hand.push(letter);
-      let index = letterPoolList.indexOf(letter);
-      letterPoolList.splice(index, 1);
-    }
-    return hand;
+  // helper function to select a random letter from letterPoolList
+  const selectRandomLetter = (letterPoolList) => {
+    let letter =
+      letterPoolList[Math.floor(Math.random() * letterPoolList.length)];
+    return letter;
   };
 
-
-export const usesAvailableLetters = (input, lettersInHand) => {
-  for(let i in input){
-    if(!(lettersInHand.includes(input[i]))){
-      return false
-    }else{
-      lettersInHand.splice(input[i], 1) 
-    }
+  // builds a hand of 10 random letters
+  for (let i = 0; i < 10; i++) {
+    const letter = selectRandomLetter(letterPoolList);
+    hand.push(letter);
+    let index = letterPoolList.indexOf(letter);
+    letterPoolList.splice(index, 1);
   }
-  return true
-  
+  return hand;
 };
 
-
-export const scoreWord = (word) => {
-  // Implement this method for wave 3
-  let score = 0
-  
-    for (let i = 0; i<word.length; i++) {
-      score += adagramsPoints[word.toUpperCase()[i]]
-    }
-    if (word.length >= 7){
-      score += 8
-    }
-    return score
-};
-
-export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
-  const wordPoints = {}
-  const winnerDict = {}
-  for (const i in words){
-    wordPoints[words[i]] = scoreWord(words[i])
-    
-  }
- 
-  let points = (Object.values(wordPoints));
-  let winner = Math.max(...points);
-  for(const [key, value] of Object.entries(wordPoints)) {
-    if(winner === value){
-      winnerDict["word"] = key
-      winnerDict["score"] = value
-    }
-  }
-  return winnerDict
-};
